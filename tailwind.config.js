ae0d63fd3a25f910c5f61cf5abf894b662277565
PakshP <pakshpatel1@gmail.com>
2024-05-01 01:30:26 -0400

    new navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#f7f7f7',
+        gray: {
+          900: '#202225',
+          800: '#2F3136',
+          700: '#36393F',
+          600: '#4F545C',
+          400: 'D4D7DC',
+          300: '#E3E5E8',
+          200: '#E9EBEF',
+          100: '#F2F3F5',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
