ef8d9a2539db326e56ba2488c11ed65254fe4234
PakshP <pakshpatel1@gmail.com>
2024-05-15 15:11:45 -0400

    updates

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import profile from '../assets/IMG_4122.jpg';
 
 const ContentContainer = () => {
     const homeRef = useRef(null);
@@ -9,25 +10,21 @@ const ContentContainer = () => {
     return (
         <div className='block items-center w-full h-screen ml-20'>
             <div id='welcome-bar' className='welcome-bar'>
-                <h1 className='m-2 ml-3 text-left flex-1'>Welcome to My Portfolio!</h1>
+                <h1 className='m-2 ml-3 text-left flex-1 font-mono'>Welcome to My Portfolio!</h1>
                 <div className='flex '>
-                    <img alt='profile' src='https://avatars.githubusercontent.com/u/77445921?v=4' 
+                    <img alt='profile' src={profile} 
                          className='rounded-full h-10 w-10 m-2 mr-3' />
                 </div>
             </div>
-            <div id='line' className='h-1 bg-gray-900' />
             <div ref={homeRef} id='home' className='content1 h-full'>
                 <h1>Home</h1>
             </div>
-            <div id='line' className='h-1 bg-gray-900' />
             <div ref={aboutRef} id='about' className='content2 h-full'>
                 <h1>About</h1>
             </div>
-            <div id='line' className='h-1 bg-gray-900' />
             <div ref={skillsRef} id='skills' className='content1 h-full'>
                 <h1>Skills</h1>
             </div>
-            <div id='line' className='h-1 bg-gray-900' />
             <div ref={contactRef} id='contact' className='content2 h-full'>
                 <h1>Contact</h1>
             </div>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import logo from '../assets/Logo.png';
+import logo from '../assets/logo-home.png';
 import { FaHome, FaInfoCircle, FaAddressBook } from 'react-icons/fa';
 import { FaGear, FaLinkedinIn, FaGithub } from 'react-icons/fa6';
 import { IoRemoveOutline } from 'react-icons/io5';
 import PropTypes from 'prop-types';
 
 const SideBar = () => {
+    // eslint-disable-next-line
     const [ activeSection, setActiveSection] = useState('');
+
+    
 
     const handleIconClick = (section) => {
         setActiveSection(section);
@@ -16,7 +19,7 @@ const SideBar = () => {
     return (
         <div className='fixed top-0 left-0 h-screen w-20 m-0
                         flex flex-col bg-primary text-secondary shadow-2xl'>
-            <div className='rounded-3xl bg-white relative flex items-center justify-center h-14 w-14 mt-5 mb-2 mx-auto shadow-lg'>
+            <div className='rounded-full relative flex items-center justify-center h-14 w-14 mt-5 mb-2 mx-auto'>
                 <img src={logo} alt='logo' className='logo'/>
             </div>
             <IoRemoveOutline size='28' className='relative flex items-center justify-center mt-2 mb-2 mx-auto' />
